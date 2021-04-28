0c428ebd41d17cff9e37e8c1683c046f0c69174f
joshuabenker <joshuabenker@joshua.joshua>
2021-04-28 15:00:20 +0200

    comments added

--- calculator.py
+++ calculator.py
@@ -11,6 +11,7 @@ class Calculator (QMainWindow):
         self.ui = uic.loadUi("calculator.ui", self)
         self.init_buttons()
 
+    # checks if buttons got clicked. calls the 'button_input' func
     def init_buttons(self):
         self.ui.button_zero.clicked.connect(lambda x: self.button_input('0'))
         self.ui.button_one.clicked.connect(lambda x: self.button_input('1'))
@@ -32,27 +33,26 @@ class Calculator (QMainWindow):
         self.ui.button_clear.clicked.connect(lambda x: self.button_input('c'))
         self.ui.button_delete.clicked.connect(lambda x: self.button_input('d'))
 
+    # logging function. prints out the input character to stdout
     def calc_log(message):
         def log_event(func):
             def logging(self, text):
-                self.log_message(message, text)
+                numbers_operators = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '-', '*', '/']
+                if text in numbers_operators:
+                    sys.stdout.write(message + text + "\n")
+                elif text == "c":
+                    sys.stdout.write(message + text + ". Input got cleared. \n")
+                elif text == "d":
+                    sys.stdout.write(message + text + ". Last character deleted. \n")
+                elif text == "=":
+                    to_calculate = self.ui.result.text()
+                    solution = eval(to_calculate)
+                    sys.stdout.write(message + text + ". Result: " + str(solution) + "\n")
                 func(self, text)
             return logging
-        return log_event
+        return log_event        
 
-    def log_message(self, message, text):
-        numbers_operators = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '-', '*', '/']
-        if text in numbers_operators:
-            sys.stdout.write(message + text + "\n")
-        elif text == "c":
-            sys.stdout.write(message + text + ". Input got cleared. \n")
-        elif text == "d":
-            sys.stdout.write(message + text + ". Last character deleted. \n")
-        elif text == "=":
-            to_calculate = self.ui.result.text()
-            solution = eval(to_calculate)
-            sys.stdout.write(message + text + ". Result: " + str(solution) + "\n")
-
+    # checks if a key got pressed 
     def keyPressEvent(self, event):
         numbers_operators = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '-', '*', '/', '=']
         if event.text() in numbers_operators:
@@ -64,14 +64,17 @@ class Calculator (QMainWindow):
         elif event.key() == QtCore.Qt.Key_Return:
             self.key_input('=')
 
+    # calls the log function when a key got pressed and calls the calculator_output func
     @calc_log("key pressed: ")
     def key_input(self, char):
         self.calculator_output(char)
 
+    # calls the log function when a button got pressed and calls the calculator_output func
     @calc_log("button pressed: ")
     def button_input(self, char):
         self.calculator_output(char)
 
+    # writes the characters to the ui and calculates the result
     def calculator_output(self, char):
         if char == '=':
             to_calculate = self.ui.result.text()
